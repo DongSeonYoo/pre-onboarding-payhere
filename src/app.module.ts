00d81bd96326733b4c5dcdf2a08717ce3b2bcb07
ehdtjs0612 <inko51366@naver.com>
2023-12-23 23:55:30 +0900

    feat: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './config/typeorm.config';
+import { LedgerController } from './api/ledger/ledger.controller';
+import { LedgerModule } from './api/ledger/ledger.module';
+import { AuthModule } from './api/auth/auth.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), LedgerModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
