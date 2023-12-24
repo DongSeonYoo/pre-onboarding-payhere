7c4feaa7040023a3f971f11e663da4e93c4395bc
ehdtjs0612 <inko51366@naver.com>
2023-12-24 20:09:39 +0900

    feat: add refresh token roatate function

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import { HashingModule } from 'src/common/hashing/hashing.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [HashingModule, TypeOrmModule.forFeature([UserEntity])],
--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { IAccessTokenPayload } from './interface/access-payload.interface';
 import env from 'src/config/env.config';
@@ -9,9 +9,10 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly jwtService: JwtService,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+
+    private readonly jwtService: JwtService,
   ) {}
 
   /**
@@ -21,9 +22,13 @@ export class AuthService {
     const accessToken = await this.generateAccessToken({ id: userId, email });
     const refreshToken = await this.generateRefreshToken({ id: userId });
 
+    // 데이터베이스에 refresh token 업데이트
+    await this.setRefreshToken(userId, refreshToken);
+
     return { accessToken, refreshToken };
   }
 
+  // 액세스 토큰 생성
   async generateAccessToken(payload: IAccessTokenPayload): Promise<string> {
     return this.jwtService.signAsync(payload, {
       secret: env.jwt.access_secret,
@@ -31,21 +36,56 @@ export class AuthService {
     });
   }
 
-  // 리프레시 토큰 생성 시 데이터베이스에 저장
+  // 리프레시 토큰 생성
   async generateRefreshToken(payload: IRefreshTokenPayload): Promise<string> {
-    const userId = payload.id;
     const refreshToken = await this.jwtService.signAsync(payload, {
       secret: env.jwt.refresh_secret,
       expiresIn: env.jwt.refresh_expires,
     });
 
-    // 데이터베이스에 업데이트
-    await this.userRepository.update(userId, {
-      refreshToken,
-    });
-
     return refreshToken;
   }
 
-  async setRefreshToken(userId: number, refreshToken: string) {}
+  // 리프레시 토큰을 데이터베이스에 저장
+  async setRefreshToken(userId: number, refreshToken: string) {
+    return this.userRepository.update(userId, {
+      refreshToken,
+    });
+  }
+
+  // 리프레시 토큰을 받아 액세스 토큰을 재발급한다
+  async rotateToken(userId: number, recivedRefreshToken: string) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    // 1. 저장된 refresh token이 없을 떄
+    if (!user || !user.refreshToken) {
+      throw new UnauthorizedException('인증 실패');
+    }
+
+    // 2. 저장된 refresh token이 유효하지 않을 때
+    // -> 이건 근데 이미 refresh guard에서 이미 검증 했기 때문에 저장된 refresh token과 동일하면 검증도 통과함
+
+    // 3. 받은 refresh token과 저장된 refresh token과 일치하지 않을 때
+    if (recivedRefreshToken !== user.refreshToken) {
+      throw new UnauthorizedException('인증 실패');
+    }
+
+    const { accessToken, refreshToken } =
+      await this.getAccessTokenAndRefreshToken(user.id, user.email);
+
+    // 4. 새로 발급 한 refresh token을 데이터베이스에 저장
+    await this.setRefreshToken(userId, refreshToken);
+
+    return { accessToken, refreshToken };
+  }
+
+  async remomveRefreshToken(userId: number) {
+    return this.userRepository.update(userId, {
+      refreshToken: null,
+    });
+  }
 }
