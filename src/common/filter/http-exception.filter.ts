67fa6fa2bd5708b3076d780f0ce47073e6fe9db3
ehdtjs0612 <inko51366@naver.com>
2023-12-26 15:16:03 +0900

    feat: global exception filter 적용 (common response)

---
+++ src/common/filter/http-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { instanceToPlain } from 'class-transformer';
+import { Request, Response } from 'express';
+import { ResponseEntity } from '../dto/common-response.dto';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(e: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res: Response = ctx.getResponse();
+
+    const statusCode = e.getStatus();
+
+    res
+      .status(statusCode)
+      .json(instanceToPlain(ResponseEntity.ERROR_WITH(statusCode, e.message)));
+  }
+}
